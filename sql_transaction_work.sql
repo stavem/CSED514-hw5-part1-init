aca464fc3c3bbfe3a4e06aab4ed0d6b9bf5a8135
Karl Stavem <klastavem@gmail.com>
2021-05-20 14:50:39 -0700

    Added sql files

--- sql_transaction_work.sql
+++ sql_transaction_work.sql
@@ -1,27 +1,37 @@
-
-
 BEGIN TRAN reserveappt
-	DECLARE @status INT
-	
-	-- Confirm the slot status is 0
-	SET @status = (SELECT SlotStatus FROM CareGiverSchedule WHERE CaregiverSlotSchedulingId = 2)	
-
-	-- CREATE INITIAL ENTRY INTO VaccineAppt Table AND FLAG PATIENT as queued for 1 dose
-	IF (@status = 0) 
-		BEGIN
-		INSERT INTO [dbo].[VaccineAppointments] 
-		([VaccineName],[PatientId],[CaregiverId],[ReservationDate],
-		[ReservationStartHour],[ReservationStartMinute],[AppointmentDuration],
-		[SlotStatus],[DateAdministered],[DoseNumber]) 
-		VALUES 
-		('pfizer',1, 2,'2021-05-18', 10, 0, 15, 3, '2021-05-18', 1)
-		UPDATE Patients SET VaccineStatus = 1 WHERE PatientId = 1
-		COMMIT TRAN reserveappt
-		END
-	ELSE
-		BEGIN
-		ROLLBACK TRAN reserveappt
-		END
+                        DECLARE @status INT
+                        
+                        -- Confirm the slot status is 0
+                        SET @status = (SELECT SlotStatus FROM CareGiverSchedule 
+                        WHERE CaregiverSlotSchedulingId = 2)	
+                        
+                        -- CREATE INITIAL ENTRY INTO VaccineAppt Table AND FLAG PATIENT as queued for 1 dose
+                        IF (@status = 1) 
+                            BEGIN
+INSERT INTO [dbo].[VaccineAppointments] 
+([VaccineName],[PatientId],[CaregiverId],[ReservationDate],
+[ReservationStartHour],[ReservationStartMinute],[AppointmentDuration],
+[SlotStatus],[DoseNumber]) 
+SELECT
+'Pfizer',
+1, 
+CaregiverId,
+WorkDay, 
+SlotHour, 
+SlotMinute, 
+15, 
+1, 
+1
+FROM
+CareGiverSchedule
+WHERE CaregiverSlotSchedulingId = 2
+                            UPDATE Patients SET VaccineStatus = 1 WHERE PatientId = 2
+                            COMMIT TRAN reserveappt
+                            END
+                        ELSE
+                            BEGIN
+                            ROLLBACK TRAN reserveappt
+                            END
 
 BEGIN TRAN nextappt
 
@@ -39,7 +49,7 @@ BEGIN TRAN nextappt
 		SELECT 
 		'pfizer', 
 		1, 
-		2, 
+		CaregiverId, 
 		WorkDay, 
 		SlotHour, 
 		SlotMinute,
